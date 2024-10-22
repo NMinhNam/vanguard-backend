0a61263ed79e26fd46aa6876dee4bb81f31dc71d
Nguyen Minh Nam <minhnam1810@gmai.com>
2024-10-23 00:15:16 +0700

    feat(TrinhDo): add file

---
+++ src/main/java/com/fpt/vanguard/mapper/mapstruct/TrinhDoMapstruct.java
@@ -0,0 +1,7 @@
+package com.fpt.vanguard.mapper.mapstruct;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TrinhDoMapstruct {
+}
---
+++ src/main/java/com/fpt/vanguard/service/impl/TrinhDoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.fpt.vanguard.service.impl;
+
+import com.fpt.vanguard.service.TrinhDoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TrinhDoServiceImpl implements TrinhDoService {
+}

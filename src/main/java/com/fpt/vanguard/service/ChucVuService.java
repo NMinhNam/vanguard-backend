f877dff85dc881109dabbac6da4e0eb9fba68ec5
Nguyen Minh Nam <minhnam1810@gmai.com>
2024-10-25 13:37:12 +0700

    feat(ChucVu): api get all

---
+++ src/main/java/com/fpt/vanguard/service/ChucVuService.java
@@ -0,0 +1,9 @@
+package com.fpt.vanguard.service;
+
+import com.fpt.vanguard.dto.response.ChucVuDtoResponse;
+
+import java.util.List;
+
+public interface ChucVuService {
+    List<ChucVuDtoResponse> getAllChucVu();
+}

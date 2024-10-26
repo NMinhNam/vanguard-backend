c9521b40c16084d294c724d4b2cbe2aeeb8fa158
Nguyen Minh Nam <minhnam1810@gmai.com>
2024-10-26 11:19:06 +0700

    feat(TuyenDung): update entity

---
+++ src/main/java/com/fpt/vanguard/entity/ViTriTuyenDung.java
@@ -0,0 +1,17 @@
+package com.fpt.vanguard.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ViTriTuyenDung {
+    private String maViTriTuyenDung;
+    private String tenViTri;
+    private String maPhongBan;
+    private Integer soLuongTuyen;
+    private Integer soLuongUngTuyen;
+    private Boolean trangThai;
+    private String moTa;
+    private String yeuCau;
+}

1b970261f0d4de0b5a7bf01a9b4aff73ab7d79a0
Nguyen Minh Nam <minhnam1810@gmai.com>
2024-12-04 11:47:32 +0700

    feat(Password): open public endpoint for api

--- src/main/java/com/fpt/vanguard/config/SecurityConfig.java
+++ src/main/java/com/fpt/vanguard/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
             "/api/v1/auth/refresh",
             "/api/v1/users/register",
             "/api/v1/otp/validate",
+            "/api/v1/auth/forgot-password",
             "/v3/api-docs/**",
             "/swagger-ui/**"
     };

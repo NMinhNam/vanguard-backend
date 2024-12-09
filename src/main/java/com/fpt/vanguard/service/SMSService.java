7106b10c3ad83cb51d6dea8154fb958c48ac28ce
Nguyen Minh Nam <minhnam1810@gmai.com>
2024-12-10 01:25:54 +0700

    feat(Meeting): api notify SMS

--- src/main/java/com/fpt/vanguard/service/SMSService.java
+++ src/main/java/com/fpt/vanguard/service/SMSService.java
@@ -1,7 +1,7 @@
-package com.fpt.vanguard.service;
-
-import com.fpt.vanguard.dto.request.SMSDtoRequest;
-
-public interface SMSService {
-    String sendSMS(SMSDtoRequest request);
-}
+//package com.fpt.vanguard.service;
+//
+//import com.fpt.vanguard.dto.request.SMSDtoRequest;
+//
+//public interface SMSService {
+//    String sendSMS(SMSDtoRequest request);
+//}

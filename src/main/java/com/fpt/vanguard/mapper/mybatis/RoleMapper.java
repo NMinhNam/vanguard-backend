628699ab0e93c7b39b7fd86a05fbcaa70ed7cd94
hoangha <hoangha01627381018@gmail.com>
2024-12-13 15:36:48 +0700

    feat(role): build api get all role

--- src/main/java/com/fpt/vanguard/mapper/mybatis/RoleMapper.java
+++ src/main/java/com/fpt/vanguard/mapper/mybatis/RoleMapper.java
@@ -3,6 +3,9 @@ package com.fpt.vanguard.mapper.mybatis;
 import com.fpt.vanguard.entity.Role;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface RoleMapper {
+    List<Role> getAll();
 }
